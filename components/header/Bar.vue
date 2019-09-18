1c8d83f5bf93321abc5330803da9e2b77a8d8e58
louis1001 <luisal1207@gmail.com>
2019-09-17 20:47:47 -0600

    Basic header structure

---
+++ components/header/Bar.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="top-bar">
+    <img src="@/assets/UNI.svg" alt="UNI"></img>
+    <h1>RUACS | Ingenieria de Sistemas</h1>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.top-bar{
+  position: fixed;
+  background: #474747;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: 0;
+  height: 60px;
+  min-width: 70vw;
+  left: calc(35vw/2);
+  /* padding: 0 50px; */
+  box-shadow: 0 3px 0 #7979798f;
+}
+
+.top-bar h1{
+  margin-left: 20px;
+}
+</style>
---
+++ components/header/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <Bar />
+  </header>
+</template>
+
+<script>
+import Bar from './Bar'
+export default {
+  components: {
+    Bar
+  }
+}
+</script>
+
+<style scoped>
+/* Header CSS */
+header{
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+}
+</style>
