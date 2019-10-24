0121ce567559fc72e376490a208ba1362fa8a5dd
louis1001 <luisal1207@gmail.com>
2019-10-23 20:17:23 -0600

    Flecha indicativa en Presentacion y correccion de estilos.

--- components/header/Presentation.vue
+++ components/header/Presentation.vue
@@ -1,27 +1,49 @@
 <template>
 <section id="presentation">
-      <div id="top-bar">
-       <nuxt-link id="link-bar" to="/">
-    <img src="@/assets/UNI.svg" alt="UNI" id="icon-uni"></img>
-    <h2>RUACS | Ingenieria de Sistemas</h2>
-    </nuxt-link>
-  </div>
+    <div id="top-bar">
+      <nuxt-link id="link-bar" to="/">
+        <img src="@/assets/UNI.svg" alt="UNI" id="icon-uni"></img>
+        <h2>RUACS | Ingenieria de Sistemas</h2>
+      </nuxt-link>
+    </div>
     <div id="info">
       <div>
-      <h1>{{getPresent.title}}</h1>
-      <p>
-        {{getPresent.info}}
-      </p>
-    </div>
+        <h1>{{getPresent.title}}</h1>
+        <p>
+          {{getPresent.info}}
+        </p>
+      </div>
     </div>
     <img v-if="getPresent.img" :src="require(`@/assets/present/${getPresent.img}`)" id="Background-image" />
+    <img src="@/assets/arrow-down-solid.svg" id="arrow-down" @click="scrollPage" />
   </section>
 </template>
 <script>
 import { mapGetters } from "vuex";
 export default {
+  created() {
+    requestAnimationFrame(this.doParallax)
+  },
   computed: {
     ...mapGetters(["getPresent"])
+  },
+  methods: {
+    doParallax() {
+      const infoSec = document.getElementById('info')
+      const imgSec = document.getElementById('Background-image')
+
+      const offSetWindow = -(window.pageYOffset * 0.4) | 0
+      const offsetOpacity = window.pageYOffset/window.innerHeight
+
+      infoSec.style.transform = 'translateY(' + offSetWindow + 'px)'
+      imgSec.style.transform = 'translateY(' + offSetWindow + 'px)'
+      infoSec.style.opacity = 1-offsetOpacity*1.5
+      imgSec.style.opacity = 1- offsetOpacity*1.5
+      requestAnimationFrame(this.doParallax)
+    },
+    scrollPage(){
+      window.scrollTo(0, window.innerHeight)
+    }
   }
 };
 </script>
@@ -37,6 +59,7 @@ export default {
   grid-template-rows: auto 1fr;
   color: #ffffff;
   margin: 0;
+  padding: 0 20px;
 }
 
 #presentation > #top-bar {
@@ -78,6 +101,16 @@ export default {
   opacity: 0.9;
 }
 
+#presentation > #arrow-down{
+  /* transform: scale(); */
+  top: calc(100% - 80px);
+  left: 50%;
+  position: absolute;
+  opacity: 0.5;
+  animation: move-up-down 3s cubic-bezier(0.68, 0.57, 0.6, 1.56) infinite;
+  transform-origin: center;
+}
+
 #presentation > #info {
   grid-column: 1;
   grid-row: 2;
@@ -101,6 +134,7 @@ export default {
   margin: 0 auto;
   font-size: 1em;
 }
+
 @media screen and (min-width: 1281px) and (max-width: 1920px) {
 #top-bar > a {
 
@@ -155,6 +189,33 @@ export default {
 #icon-uni {
   display: none;
 }
+}
+
+@keyframes move-up-down {
+  0%{
+    opacity: 0;
+    transform: translateY(-40px);
+    width: 30px;
+  }
+  85%{
+    opacity: 0.8;
+    transform: translateY(0);
+    width: 35px;
+  }
+  86%{
+    opacity: 0.8;
+    transform: translateY(0);
+  }
+
+  /* 92%{
+    transform: translateY(-10px);
+  } */
+
+  100%{
+    opacity: 0;
+    width: 35px;
+    /* transform: translateY(0); */
+  }
 }
 
 </style>
